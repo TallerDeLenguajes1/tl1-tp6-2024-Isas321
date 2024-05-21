a47a4e4c20abf088d56e4e27cf81363d0fb69b97
Javier Alfredo Isasmendi <javier.isas321@gmail.com>
2024-05-21 01:43:17 -0300

    configuracion de json + primer codigo

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a;
+int b;
+    a=10;
+b=a;
+
+Console.WriteLine("valor de a: "+a);
+Console.WriteLine("valor de b: "+b);
